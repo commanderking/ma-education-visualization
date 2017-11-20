2542235e9619ae13ebe36ffb242e37e8aceec61c
commanderking <jek582@mail.harvard.edu>
2017-11-19 20:21:37 -0500

    refactor some methods out of render view

--- visualizations/discipline/index.js
+++ visualizations/discipline/index.js
@@ -2,7 +2,7 @@
     const dataSource = "data.json";
     const disciplineConsts = {
       STUDENTS: 'students',
-      STUDENTS_DISCIPLINED: 'studentsDisciplined',
+      STUDENTS_DISCIPLINED: 'Students Disciplined',
       IN_SCHOOL_SUSPENSION: 'inSchoolSuspension',
       OUT_SCHOOL_SUSPENSION: 'outSchoolSuspension',
       EXPULSION: 'expulsion',
@@ -10,20 +10,29 @@
       EMERGENCY_REMOVAL: 'emergencyRemoval',
     }
 
-    const colorMap = {
-      "Boston": "#98abc5",
-      "Boston Charter Schools" : "#8a89a6"
+    const keyConstants = {
+      CHARTER_SCHOOLS: "Charter Schools",
+      TRADITIONAL_PUBLIC_SCHOOLS: "Traditional Public",
     }
+
+    const colorMap = {
+      [keyConstants.TRADITIONAL_PUBLIC_SCHOOLS]: "#98abc5",
+      [keyConstants.CHARTER_SCHOOLS]: "#8a89a6"
+    }
+
     const svgMargins = {top: 20, right: 20, bottom: 70, left: 80};
-    let enrollmentData = [];
-    let x;
+    let x0;
+    let x1;
     let y;
+    let z;
     let g;
     let svg;
     let schoolSelectorWrapper;
     let allSchoolNames = [];
     let width;
     let height;
+    let currentCategories = ["discipline"];
+    let keys = [keyConstants.TRADITIONAL_PUBLIC_SCHOOLS, keyConstants.CHARTER_SCHOOLS]
 
     let model = {};
 
@@ -74,105 +83,56 @@
       return charterSummaryData;
     }
 
+    const createCategoryData = (charterData, traditionalData, category) => {
+      return {
+        name: category,
+        [keyConstants.CHARTER_SCHOOLS]: charterData[category] / charterData.students,
+        [keyConstants.TRADITIONAL_PUBLIC_SCHOOLS]: traditionalData[category] / traditionalData.students
+      }
+    }
+
+    // Creates final data structure before feeding into d3
+    const createProcessedData = (charterData, traditionalData, categoriesArray) => {
+      const processedData = categoriesArray.map((category) => {
+        return createCategoryData(charterData, traditionalData, category);
+      });
+
+      return processedData;
+    };
+
     // SVG related additions
     const view = {
-      renderData: (data) => {
+      initializeSvg: () => {
+          svg = d3.select("svg"),
+          margin = {top: 20, right: 20, bottom: 30, left: 100},
+          width = +svg.attr("width") - margin.left - margin.right,
+          height = +svg.attr("height") - margin.top - margin.bottom,
+          g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      },
 
-        // Set up d3 scales
-        var svg = d3.select("svg"),
-            margin = {top: 20, right: 20, bottom: 30, left: 100},
-            width = +svg.attr("width") - margin.left - margin.right,
-            height = +svg.attr("height") - margin.top - margin.bottom,
-            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        var x0 = d3.scaleBand()
-            .rangeRound([50, width/3])
+      setScales: () => {
+        x0 = d3.scaleBand()
+            .rangeRound([50, width - 150])
             .paddingInner(0.01);
 
-        var x1 = d3.scaleBand()
-            .padding(0.05);
+        x1 = d3.scaleBand()
+            .padding(0);
 
-        var y = d3.scaleLinear()
+        y = d3.scaleLinear()
             .rangeRound([height, 0]);
 
-        var z = d3.scaleOrdinal()
-            .range(["#98abc5", "#8a89a6"]);
+        z = d3.scaleOrdinal()
+            .range(
+              [colorMap[keyConstants.TRADITIONAL_PUBLIC_SCHOOLS], colorMap[keyConstants.CHARTER_SCHOOLS]]);
+      },
 
-        // Manipulating data to get 2015 all student data
-        const allStudentsData = filterByStudentType(data, 'all');
-
-        const studentFifteenData = filterByYear(data, 2015);
-
-        console.log(studentFifteenData);
-
-        const charterSchools = filterByDistrictType(studentFifteenData, 'Charter');
-        const charterSummaryData = sumCharterSchools(charterSchools);
-
-        const publicDistrictSummaryData = filterByDistrictType(studentFifteenData, 'Traditional Public');
-
-        const combinedSummary = [charterSummaryData, publicDistrictSummaryData[0]];
-
-        console.log(combinedSummary);
-
-
-        // Start plotting
-
-        x0.domain(combinedSummary.map(function(district) { 
-          return district.districtName; 
-        }));
-
-        y.domain([0, .25]);
-
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x0));
-
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(5, '%'))
-
-        g.selectAll(".bar")
-          .data(combinedSummary)
-          .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { 
-              return x0(d.districtName);
-            })
-            .attr("y", height)
-            .attr("width", x0.bandwidth())
-            .attr("height", 0)
-            .attr("fill", function(district) {
-              console.log(district);
-              return colorMap[district.districtName];
-            })
-            .transition()
-            .duration(1000)
-            .attr("height", function(district) { 
-              const percentDisciplined = district.studentsDisciplined / district.students
-              return height - y(percentDisciplined); 
-            })
-            .attr("y", function(district) {
-              const percentDisciplined = district.studentsDisciplined / district.students
-              return y(percentDisciplined); 
-            })
-
-        // Append y axis label
-        g.append("text")
-            .attr("class", "label-y")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - (svgMargins.left))
-            .attr("x",0 - (height / 2))
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("% Students Disciplined");
-
+      renderLegend: () => {
         const legend = g.append("g")
             .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
+            .attr("font-size", 14)
             .attr("text-anchor", "end")
           .selectAll("g")
-          .data(["Boston Charter Schools", "Boston"].reverse())
+          .data(keys.reverse())
           .enter().append("g")
             .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
@@ -189,6 +149,90 @@
             .text(function(d) { 
               return d; 
             });
+      },
+
+      renderYLabel: () => {
+        g.append("text")
+            .attr("class", "label-y")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - (svgMargins.left))
+            .attr("x",0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("% Students Disciplined");
+      },
+
+      renderData: (data) => {
+        // Set up d3 scales
+        view.initializeSvg();
+
+        view.setScales();
+
+        // Manipulating data to get 2015 all student data
+        const allStudentsData = filterByStudentType(data, 'all');
+
+        const studentFifteenData = filterByYear(data, 2015);
+        const charterSchools = filterByDistrictType(studentFifteenData, 'Charter');
+        const charterSummaryData = sumCharterSchools(charterSchools);
+        const publicDistrictSummaryData = filterByDistrictType(studentFifteenData, 'Traditional Public');
+
+        const processedData = createProcessedData(charterSummaryData, publicDistrictSummaryData[0], [disciplineConsts.STUDENTS_DISCIPLINED]);
+
+
+        const tempData = {
+          "categoryName": "suspensions",
+          [keyConstants.CHARTER_SCHOOLS]: 0.10,
+          [keyConstants.TRADITIONAL_PUBLIC_SCHOOLS] : 0.15
+        }
+
+        // Start plotting
+
+        x0.domain(processedData.map(function(category) { 
+          return category.name; 
+        }));
+
+        x1.domain(keys).rangeRound([0, x0.bandwidth()]);
+
+        y.domain([0, .25]);
+
+        g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x0));
+
+        g.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(y).ticks(5, '%'))
+
+        g.append("g")
+          .selectAll("g")
+          .data(processedData)
+          .enter().append("g")
+            .attr("transform", function(d) { 
+              return "translate(" + x0(d.name) + ",0)"; 
+            })
+          .selectAll("rect")
+          .data(function(d) { 
+            const retval = keys.map(function(key) { 
+              return {key: key, value: d[key]}; 
+            });
+            return retval;
+          })
+          .enter().append("rect")
+            .attr("x", function(d) { 
+              return x1(d.key); 
+            })
+            .attr("y", function(d) { 
+              return y(d.value); 
+            })
+            .attr("width", x1.bandwidth())
+            .attr("height", function(d) { return height - y(d.value); })
+            .attr("fill", function(d) { 
+              return z(d.key); 
+            });
+
+        view.renderLegend();
+        view.renderYLabel();
       }
     }
 
